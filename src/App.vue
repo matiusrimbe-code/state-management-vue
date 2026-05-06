822629d5a9a83387912583c18641a768c99aba49
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2026-05-06 18:40:39 +0800

    use ref

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const red = ref(0)
+const green = ref(0)
+const blue = ref(0)
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main :style="{ backgroundColor: `rgb(${red},${green},${blue})` }">
+    <div class="container">
+      <input type="text" v-model="red" class="input-text" placeholder="Nilai Red" />
+      <input type="text" v-model="green" class="input-text" placeholder="Nilai Green" />
+      <input type="text" v-model="blue" class="input-text" placeholder="Nilai Blue" />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  background-color: gray;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.input-text {
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+}
+</style>
